575cc5b622d7345f8ab5b3f7e7bc172e2c7dc309
R0ut <kkaacczzoorr@gmail.com>
2017-01-12 10:18:48 +0100

    Add project files.

---
+++ testowycalc/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testowycalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
